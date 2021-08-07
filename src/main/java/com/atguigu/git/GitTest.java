3b18d9466c25d96319eb392b03053df1d3d8f374
D4C <abdetianshi@163.com>
2021-08-07 16:57:55 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @ClassName GitTest
+ * @Description TODO
+ * @Author zenghw
+ * @Date 2021/8/7 16:42
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
